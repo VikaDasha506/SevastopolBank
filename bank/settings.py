3ce01086e69361ca0d9e768e2e3300cf545adf68
VikaDasha506 <Manin_lt29@top-academy.ru>
2024-06-24 01:41:45 +0300

    добавили ALLOWED_HOSTS

--- bank/settings.py
+++ bank/settings.py
@@ -29,9 +29,10 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*', 'vikadasha506-sevastopolbank-39c6.twc1.net']
 
 INTERNAL_IPS = [
+    '127.0.0.1',
 ]
 
 
--- bank/tasks.py
+++ bank/tasks.py
@@ -12,10 +12,18 @@ def fetch_exchange_rates():
         data = response.json()
         # Кешируем данные на 3 часа (10800 секунд)
         cache.set('exchange_rates', data, timeout=10800)
+        return data
     else:
         # Обработка ошибок или логирование
         print('Failed to fetch exchange rates')
+       # Получаем данные из кеша
+    exchange_rates = cache.get('exchange_rates')
 
+        # Проверяем, есть ли данные
+    if exchange_rates:
+        print(exchange_rates)
+    else:
+         print('Данные о курсах валют отсутствуют в кеше.')
 
 # python manage.py shell
 # from django.core.cache import cache
