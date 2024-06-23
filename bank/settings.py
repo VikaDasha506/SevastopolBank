82fcad1f49d0779bfe3bfc260f6a055951987682
VikaDasha506 <Manin_lt29@top-academy.ru>
2024-06-23 21:46:06 +0300

    добавили celery

--- bank/settings.py
+++ bank/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['https://vikadasha506-sevastopolbank-7614.twc1.net','*']
+ALLOWED_HOSTS = ['vikadasha506-sevastopolbank-7614.twc1.net','*']
 
 INTERNAL_IPS = [
     '127.0.0.1',
