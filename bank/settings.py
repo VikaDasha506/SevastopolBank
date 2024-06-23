1e1aff4e783ad6d0a54253e04582e4bb7958f6b3
VikaDasha506 <Manin_lt29@top-academy.ru>
2024-06-24 01:49:16 +0300

    добавили ALLOWED_HOSTS

--- bank/settings.py
+++ bank/settings.py
@@ -29,7 +29,10 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*', 'vikadasha506-sevastopolbank-39c6.twc1.net']
+ALLOWED_HOSTS = [
+    'vikadasha506-sevastopolbank-39c6.twc1.net',
+    '*',
+]
 
 INTERNAL_IPS = [
     '127.0.0.1',
