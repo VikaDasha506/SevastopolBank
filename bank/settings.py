796a6c83a895eb80f0f6cb7f4c8dfa7dad7e5ab2
VikaDasha506 <Manin_lt29@top-academy.ru>
2024-06-23 13:12:26 +0300

    Проект Банк

--- bank/settings.py
+++ bank/settings.py
@@ -29,10 +29,9 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 INTERNAL_IPS = [
-    '127.0.0.1',
 ]
 
 # Application definition
